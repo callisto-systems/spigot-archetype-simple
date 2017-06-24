1de3d471c5402a237ac9d2d912723404cfc5e0eb
vasilache <mihai_vasilache@yahoo.com>
2017-06-24 12:56:00 +0300

    fix imports

--- src/main/resources/archetype-resources/src/main/java/__pluginClassName__.java
+++ src/main/resources/archetype-resources/src/main/java/__pluginClassName__.java
@@ -3,9 +3,7 @@
 #set( $symbol_escape = '\' )
 package ${package};
 
-import java.util.logging.Level;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.plugin.PluginManager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
